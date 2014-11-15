a03bc3bcb21bd090a5ad303898693b8314f222cf
Caio Vinicius Marques Teixeira <caio.vinicius.teixeira@usp.br>
2014-11-15 19:45:47 -0200

    Gameover ao cair

--- Assets/AviaoController.cs
+++ Assets/AviaoController.cs
@@ -4,16 +4,20 @@ using UnityEngine.UI;
 
 public class AviaoController : MonoBehaviour {
 
-
+    //Vetor de força aplicada no avião
 	public Vector2 movimento = new Vector2(0.0f,1.0f);
 
+    //Tecla de pulo
 	public KeyCode jumpKey = KeyCode.Space;
 
+    //Referência para o canvas de gameover
 	public GameObject gameover;
 
+    //Referência para o UI Text de pontos
 	public Text pontuacaoTexto;
 	int pontuacao = 0;
 
+    //Jogador pode controlar?
 	bool canControl = true;
 
 	void FixedUpdate () {
@@ -28,10 +32,19 @@ public class AviaoController : MonoBehaviour {
 
 	void OnCollisionEnter2D(Collision2D colisao)
 	{
-		gameover.SetActive(true);
-		canControl = false;
-		//Time.timeScale = 0.0f; 	
+        gameOver();
 	}
+
+    public void OnBecameInvisible()
+    {
+        gameOver();
+    }
+
+    void gameOver()
+    {
+        gameover.SetActive(true);
+        canControl = false;	
+    }
 
 	public void tryAgain()
 	{
--- Assets/Cenario.cs
+++ Assets/Cenario.cs
@@ -3,20 +3,27 @@ using System.Collections;
 
 public class Cenario : MonoBehaviour {
 
-
+    //Prefab do obstaculo
 	public GameObject prefab;
+
+    //Intervalo entre novos obstaculos
 	public float intervalo = 2.0f;
 
+    //Valores mínimos e máximos para a posição no Eixo Y do obstaculo
 	public float min = -3;
 	public float max = 3;
 
 	void Start () {
+        //Inicia repetição da criação de obstaculos
 		InvokeRepeating("criarObstaculo", 1.0f, intervalo);
 	}
 	
+    //Instancia novo objeto
 	void criarObstaculo()
 	{
 		GameObject obj = Instantiate(prefab) as GameObject;
+
+        //Randomiza a posicao Y do obstaculo
 		obj.transform.position = new Vector2(obj.transform.position.x,Random.Range(min,max));
 	}
 }
